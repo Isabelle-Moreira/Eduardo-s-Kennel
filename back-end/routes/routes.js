0256f86119befb094631680140a27b09f8578075
Isabelle-Moreira <106937059+Isabelle-Moreira@users.noreply.github.com>
2024-01-26 00:19:20 -0300

    Realização do população das tabelas do BD

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -3,17 +3,42 @@ const router = express.Router()
 const Controller = require("../controllers/controller")
 
 router.post('/cadastro/petshop', (req, res) => {
-       console.log(req.body.id)
-       console.log(req.body.nome)
-       console.log(req.body.distancia_km)
       Controller.createPetshop({
       id:req.body.id,
       nome_petshop: req.body.nome,
       distancia_km: req.body.distancia_km
     }).then((Petshops) => res.send(Petshops))
       .catch((err) => {
-        console.log('Erro no cadastro do item', JSON.stringify(err))
+        console.log('Erro no cadastro do petshop', JSON.stringify(err))
         return res.status(400).send(err)
       })
   })
+
+  router.post('/cadastro/TipoCao', (req, res) => {
+   Controller.createTipoCao({
+   id:req.body.id,
+   tipo: req.body.tipo
+ }).then((TiposCaes) => res.send(TiposCaes))
+   .catch((err) => {
+     console.log('Erro no cadastro do tipo de cao', JSON.stringify(err))
+     return res.status(400).send(err)
+   })
+})
+
+router.post('/cadastro/precos', (req, res) => {
+  Controller.createPrecos({
+  id:req.body.id,
+  petshop_id: req.body.id_petshop,
+  TiposCaes_id: req.body.id_TipoCao,
+  preco_semana:req.body.preco_semana,
+  preco_fim_desemana:req.body. preco_fim_desemana
+}).then((Precos) => res.send(Precos))
+  .catch((err) => {
+    console.log('Erro no cadastro do tipo de cao', JSON.stringify(err))
+    return res.status(400).send(err)
+  })
+})
+
+
+
   module.exports = router
